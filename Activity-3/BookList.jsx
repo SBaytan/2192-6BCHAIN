dfad35e7c7e41743f08977f91700c6e8fdc33d5c
SBaytan <98503771+SBaytan@users.noreply.github.com>
2024-01-16 15:10:43 +0800

    Add files via upload

---
+++ Activity-3/BookList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function BookList() {
+        return(
+        <div id="container">
+            <h2> Books List</h2> 
+            <li> The Blackened Blade </li>  
+            <li> Pack Darling </li>  
+            <li> Lola and The Millionaires </li>  
+        </div>
+    );
+}
+
