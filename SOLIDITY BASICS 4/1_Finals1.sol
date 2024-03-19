63c4d979c1f1997364e298ecf4869c10445d1267
SBaytan <98503771+SBaytan@users.noreply.github.com>
2024-03-19 15:19:15 +0800

    1_Finals1.sol

--- SOLIDITY BASICS 4/1_Finals1.sol
+++ SOLIDITY BASICS 4/1_Finals1.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
-contract SelfDestruct {
-    event Deposit (uint amount); 
-    event Withdraw(uint amount);
-    address public owner = msg.sender;
-
-    receive() external payable { 
-        emit Deposit (msg.value);
+contract FallbackExample {
+    event FallbackReceived (address sender, uint amount); 
+    // Event to log payment received
+    event PaymentReceived (address payer, uint256 amount);
+    
+    // Fallback function
+    fallback() external payable {
+        emit FallbackReceived (msg.sender, msg.value);    
     }
-
-    function withdraw() external {
-    require(msg.sender == owner, "Not owner!"); 
-    emit Withdraw(address (this).balance);
-    selfdestruct(payable(msg.sender));
+    
+    // Function to receive Ether
+    receive() external payable {
+        emit PaymentReceived (msg.sender, msg.value);
     }
 }
 
 //BAYTAN SHEM ARDELEE PIOLEEN M.
 //WD-401
-//this is a simple withdrawal of ther that is restricted to the owner
+//this is a simple recieving of ether
