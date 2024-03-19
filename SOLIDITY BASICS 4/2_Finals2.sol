8c9cd50c137d23acc899768ba06d5ef8da2dd147
SBaytan <98503771+SBaytan@users.noreply.github.com>
2024-03-19 15:10:20 +0800

    2_Finals2.sol

--- SOLIDITY BASICS 4/2_Finals2.sol
+++ SOLIDITY BASICS 4/2_Finals2.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
+contract SelfDestruct {
+    event Deposit (uint amount); 
+    event Withdraw(uint amount);
+    address public owner = msg.sender;
+
+    receive() external payable { 
+        emit Deposit (msg.value);
+    }
+
+    function withdraw() external {
+    require(msg.sender == owner, "Not owner!"); 
+    emit Withdraw(address (this).balance);
+    selfdestruct(payable(msg.sender));
+    }
+}
+//this is a simple withdrawal of ther that is restricted to the owner
+
 contract SendEther { 
     constructor() payable {}
 
