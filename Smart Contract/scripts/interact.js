f352acf85b3c1d708171ddd1f1c2596047bb5944
SBaytan <98503771+SBaytan@users.noreply.github.com>
2024-04-06 17:38:52 +0800

    Add files via upload

---
+++ Smart Contract/scripts/interact.js
@@ -0,0 +1,30 @@
+// interact.js
+const ethers = require('ethers');
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+// For Hardhat
+const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+console.log(JSON.stringify(contract.abi));
+
+// Provider
+const alchemyProvider = new ethers.providers.JsonRpcProvider(API_URL);
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+// Contract
+const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main() {
+    const message = await helloWorldContract.message();
+    console.log("The message is: " + message);
+
+    console.log("Updating the message...");
+    const tx = await helloWorldContract.update("This is the new message.");
+    await tx.wait();
+    console.log("Message updated successfully!");
+}
+
+main();
